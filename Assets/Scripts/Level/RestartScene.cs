f039f85dcc2de4aa724c78c9b94c4c67beb7afc0
Green-Blood <bloodjey@gmail.com>
2021-06-08 14:14:06 +0500

    Commit

--- Assets/Scripts/Level/RestartScene.cs
+++ Assets/Scripts/Level/RestartScene.cs
@@ -5,9 +5,6 @@ namespace Level
 {
     public class RestartScene : MonoBehaviour
     {
-        public void Restart()
-        {
-            SceneManager.LoadScene(SceneManager.GetActiveScene().ToString());
-        }
+        public void Restart() => SceneManager.LoadScene(SceneManager.GetActiveScene().ToString());
     }
 }
